bd7115843fc1388f871e8eec1b428eeb8ad679de
yangkangkai <452306233@qq.com>
2019-03-01 15:42:06 +0800

    20190301

---
+++ courses_info99.js
@@ -0,0 +1,51 @@
+//课程数据集，课程固定，静态，不需要服务器支持。Time是将时间转为分钟，比如10小时变为600分钟，duration表示将课程时段。
+//周日在最前面
+var fjl = [
+	[
+		{ 'cousrName': 'CYCLE单车', 'TrainName': 'Miya', 'Time': '1210', 'duration': '50' },
+		{ 'cousrName': 'Hatha Yoga 哈他瑜伽', 'TrainName': 'Miya', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'Jazz爵士舞', 'TrainName': 'David', 'Time': '1220', 'duration': '50' }
+	],
+
+	[
+		{ 'cousrName': "Zumba 尊巴", 'TrainName': 'Amin', 'Time': '600', 'duration': '50' },
+		{ 'cousrName': 'TRX 悬吊训练', 'TrainName': 'Julia', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'Yin yoga 阴瑜伽', 'TrainName': 'Yiyi', 'Time': '1140', 'duration': '50' },
+		// 她家有事，请了几个月的假。
+		// { 'cousrName': 'CYCLE单车', 'TrainName': 'Joey', 'Time': '1210', 'duration': '50' },
+	],
+
+	[
+		{ 'cousrName': 'Gentle Flow 绽放·流', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
+		{ 'cousrName': 'Hatha Yoga 哈他瑜伽', 'TrainName': 'Yiyi', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'CYCLE单车', 'TrainName': 'Hunter', 'Time': '1160', 'duration': '50' },
+		{ 'cousrName': 'CYCLE单车', 'TrainName': 'Hunter', 'Time': '1210', 'duration': '50' },
+	],
+
+	[
+		{ 'cousrName': 'Yoga physical 理疗瑜伽', 'TrainName': 'Yiyi', 'Time': '600', 'duration': '50' },
+		{ 'cousrName': 'Lumbar abdomen 腰腹', 'TrainName': 'Marco', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': "Zumba 尊巴", 'TrainName': 'Amin', 'Time': '1220', 'duration': '50' },
+		{ 'cousrName': 'Hatha yoga 哈他瑜伽', 'TrainName': 'Yiyi', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'CYCLE单车', 'TrainName': 'Marco', 'Time': '1210', 'duration': '50' },
+	],
+
+	[
+		{ 'cousrName': 'Pilates 修复普拉提', 'TrainName': 'Julia', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'Sexy Butt 美臀', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
+	],
+
+	[
+		{ 'cousrName': 'Shape yoga 形体瑜伽', 'TrainName': 'Julia', 'Time': '600', 'duration': '50' },
+		{ 'cousrName': 'Bodypump 杠铃', 'TrainName': 'Marco', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'Jazz爵士舞', 'TrainName': 'David', 'Time': '1220', 'duration': '50' },
+		{ 'cousrName': 'Yin yoga 阴瑜伽', 'TrainName': 'Yiyi', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'CYCLE单车', 'TrainName': 'Marco', 'Time': '1210', 'duration': '50' },
+	],
+
+	[
+		{ 'cousrName': 'Flow yoga 流瑜伽', 'TrainName': 'Yiyi', 'Time': '840', 'duration': '50' },
+		{ 'cousrName': 'Bodycombat 搏击', 'TrainName': 'Alan', 'Time': '600', 'duration': '50' },
+		{ 'cousrName': 'Dance Yoga 舞韵瑜伽', 'TrainName': 'Yur', 'Time': '1140', 'duration': '50' }
+	]
+]
---
+++ trains_info99.js
@@ -0,0 +1,174 @@
+//团操教练
+var data_group_trains =
+{
+	trains_info:
+		[
+			{
+				en_name: "Darren",
+				cn_name: "曾钟钟",
+				position: "佑远团操主管",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201811/201811051656455936190.jpg",
+				certificates: [
+					"Schwinn单车一星认证",
+					"Train Fitness体系fight fx认证",
+					"莱美体系bodypump初始培训",
+				],
+				projects: ["bodypump", "莱美搏击", "CXWORD", "Fight FX", "PUMP FX", "Zumba", "Adidas训练课", "Schwinn"]
+			},
+			{
+				en_name: "Yiyi",
+				cn_name: "李玮秋",
+				position: "佑远团操教练",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201811/201811221441194834220.jpg",
+				certificates: [
+					"中国瑜伽师联盟高级瑜伽导师",
+					"阴瑜伽高伽导师认证",
+					"流瑜伽瑜伽导师认证",
+					"高温瑜伽瑜伽导师认证",
+				],
+				projects: ["哈他瑜伽", "阴瑜伽", "流瑜伽", "理疗瑜伽", "高温瑜伽"]
+			},
+			{
+				en_name: "Julia",
+				cn_name: "未央",
+				position: "佑远团操教练",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201812/201812151011402445200.jpg",
+				certificates: [
+					"理疗契合养生导师",
+					"哈他瑜伽注册教练",
+					"流瑜伽注册教练",
+					"阴瑜伽注册教练",
+					"热瑜伽注册教练",
+				],
+				projects: ["哈他瑜伽", "阴瑜伽", "流瑜伽", "理疗瑜伽", "高温瑜伽", "心灵疗愈", "阿斯汤加", "普拉提器械", "双语儿童"]
+			},
+			{
+				en_name: "Alan",
+				cn_name: "艾伦",
+				position: "佑远团操教练",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201902/201902221629242033740.jpg",
+				certificates: [
+					"哈他瑜伽注册教练",
+					"动感单车注册教练",
+					"杠铃操注册教练",
+					"搏击操注册教练",
+				],
+				projects: ["动感单车","杠铃操","搏击操","哈他瑜伽","核心训练"]
+			},
+			{
+				en_name: "David",
+				cn_name: "大卫",
+				position: "佑远团操教练",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201902/201902221629157971470.jpg",
+				certificates: [
+					"品诺街舞大赛齐舞季军",
+					"爱跳全国街舞大赛齐舞季军",
+					"各大电视台合作艺人",
+					"北京SDT舞蹈天团演艺部跟团嘉宾"
+				],
+				projects: ["爵士舞"]
+			},
+			{
+				en_name: "Marco",
+				cn_name: "马可",
+				position: "佑远团操教练",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201902/201902221727395371810.jpg",
+				certificates: [
+					"单车培训资格认证",
+					"杠铃培训资格认证",
+					"搏击培训资格认证",
+					"腰腹核心培训资格认证",
+					"蹦床培训资质证书",
+				],
+				projects: ["单车","杠铃操","搏击","腰腹核心","蹦床"]
+			},
+			{
+				en_name: "Joey",
+				cn_name: "灵儿",
+				position: "佑远团操教练",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201902/201902221629096250320.jpg",
+				certificates: [
+					"单车培训资格认证",
+				],
+				projects: ["单车"]
+			},
+			{
+				en_name: "Miya",
+				cn_name: "小雨",
+				position: "佑远团操教练",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201902/201902231734487493910.jpg",
+				certificates: [
+					"哈他瑜伽注册教练",
+					"空中瑜伽注册教练",
+					"单车注册教练",
+				],
+				projects: ["单车","空中瑜伽"]
+			},
+		]
+};
+
+//私人教练
+var data_person_trains =
+{
+	trains_info:
+		[
+			{
+				en_name: "Echo",
+				cn_name: "张慧丽",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201811/201811221924426906387.jpg",
+				position: "佑远私人教练",
+				certificates: [
+					"国家职业资格证书",
+					//					"红十字会急救员证",
+					"功能性训练技能鉴定证书",
+					"普拉提（康复训练）技能鉴定证书",
+					"运动康复3M拉伸技能鉴定证书",
+					//					"拳课技能鉴定证书",
+				],
+				projects: ["拉伸", "增肌", "减脂", "塑形"]
+			},
+			{
+				en_name: "Alick Li",
+				cn_name: "李文华",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201811/201811221924426750231.jpg",
+				position: "佑远私人教练",
+				certificates: [
+					"国家职业资格认证",
+					"上海红十字会急救（CPR）认证",
+					"TRX认证",
+					"极致瘦身认证",
+				],
+				projects: ["增肌", "减脂", "塑形"]
+			},
+			{
+				en_name: "Flute Wang",
+				cn_name: "王天将",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201811/201811221924426906386.jpg",
+				position: "佑远私人教练",
+				certificates: [
+					"CPR应急救护",
+					"CPT私人教练认证",
+					//					"体适能认证",
+					"运动训练师认证",
+					//					"体态评估与矫正",
+					"运动复健师认证",
+					"国家体育总局健身教练职业资格认证"
+				],
+				projects: ["减脂", "塑形", "拉伸"]
+			},
+			{
+				en_name: "Yi Wang",
+				cn_name: "王艺",
+				"img": "http://yy.rxmmd.net/Storage/master/gallery/201812/201812271022119220360.jpg",
+				position: "佑远私人教练",
+				certificates: [
+					"国家级健身指导员",
+					"AASFP高级私人教练",
+					"MFT泰拳格斗教练",
+					"苏萨坤泰拳教练",
+					"昆仑决签约选手",
+				],
+				projects: ["减脂", "塑形", "格斗"]
+			},
+		]
+};
