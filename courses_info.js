8c5663f454a5be6d76ff7821f3cfbb352b07177e
yangkangkai <452306233@qq.com>
2019-03-04 15:46:39 +0800

    20190304 1546

--- courses_info.js
+++ courses_info.js
@@ -9,7 +9,7 @@ var fjl = [
 
 	[
 		{ 'cousrName': "Zumba 尊巴", 'TrainName': 'Amin', 'Time': '600', 'duration': '50' },
-		{ 'cousrName': 'TRX 悬吊训练', 'TrainName': 'Julia', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'TRX 急速塑形', 'TrainName': 'Julia', 'Time': '1140', 'duration': '50' },
 		{ 'cousrName': 'Yin yoga 阴瑜伽', 'TrainName': 'Yiyi', 'Time': '1140', 'duration': '50' },
 		// 她家有事，请了几个月的假。
 		// { 'cousrName': 'CYCLE单车', 'TrainName': 'Joey', 'Time': '1210', 'duration': '50' },
