3c6fc42c4fb1bfd6b30bba571224c96dcc6d523a
yangkangkai <452306233@qq.com>
2019-04-01 17:58:24 +0800

    20190401

--- courses_info.js
+++ courses_info.js
@@ -2,46 +2,45 @@
 //周日在最前面
 var fjl = [
 	[
-		{ 'cousrName': 'CYCLE 减脂周末', 'TrainName': 'Miya', 'Time': '1210', 'duration': '50' },
-		{ 'cousrName': 'Hatha Yoga 哈他瑜伽', 'TrainName': 'Miya', 'Time': '1140', 'duration': '50' },
-		{ 'cousrName': 'Jazz爵士舞', 'TrainName': 'David', 'Time': '1220', 'duration': '50' }
+		{ 'cousrName': 'Dance Yoga 舞韵瑜伽', 'TrainName': 'Miya', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'Body Pump 塑形杠铃', 'TrainName': 'Miya', 'Time': '1200', 'duration': '50' },
 	],
 
 	[
 		{ 'cousrName': "Zumba 尊巴", 'TrainName': 'Amin', 'Time': '600', 'duration': '50' },
-		{ 'cousrName': 'TRX 急速塑形', 'TrainName': 'Julia', 'Time': '1140', 'duration': '50' },
-		{ 'cousrName': 'Yin yoga 阴瑜伽', 'TrainName': 'Yiyi', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'Power Flow 能量流瑜伽', 'TrainName': 'Julia', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'Yin yoga 阴瑜伽', 'TrainName': 'Yiyi', 'Time': '840', 'duration': '50' },
 	],
 
 	[
-		{ 'cousrName': 'Gentle Flow 绽放·流', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
-		{ 'cousrName': 'Hatha Yoga 哈他瑜伽', 'TrainName': 'Yiyi', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'Mat Pilates 垫上普拉提', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
+		{ 'cousrName': 'Relaxing Strectr 背痛修复', 'TrainName': 'Jack', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'Body Show 魔力瘦', 'TrainName': 'Yiyi', 'Time': '1200', 'duration': '50' },
 	],
 
 	[
 		{ 'cousrName': 'Yoga physical 理疗瑜伽', 'TrainName': 'Yiyi', 'Time': '600', 'duration': '50' },
-		{ 'cousrName': 'Lumbar abdomen 腰腹塑形', 'TrainName': 'Marco', 'Time': '1140', 'duration': '50' },
-		// { 'cousrName': "Zumba 尊巴", 'TrainName': 'Amin', 'Time': '1220', 'duration': '50' },
-		{ 'cousrName': 'Hatha yoga 哈他瑜伽', 'TrainName': 'Yiyi', 'Time': '1140', 'duration': '50' },
-		{ 'cousrName': 'CYCLE 巅峰激情', 'TrainName': 'Marco', 'Time': '1210', 'duration': '50' },
+		{ 'cousrName': 'Body Banalce 身心平衡', 'TrainName': 'ice', 'Time': '840', 'duration': '50' },
+		{ 'cousrName': 'I Yoga 炫腹纤腰', 'TrainName': 'Yiyi', 'Time': '1140', 'duration': '50' },
 	],
 
 	[
-		{ 'cousrName': 'Pilates 修复普拉提', 'TrainName': 'Julia', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'I Yoga 经络养生', 'TrainName': 'Julia', 'Time': '600', 'duration': '50' },
+		{ 'cousrName': 'Gentle Flow 绽放流', 'TrainName': 'Julia', 'Time': '1140', 'duration': '50' },
+	],
+
+	[
+		{ 'cousrName': 'Cycle 脂肪杀手', 'TrainName': 'Queenie', 'Time': '1190', 'duration': '50' },
+		{ 'cousrName': 'Jazz爵士舞', 'TrainName': 'David', 'Time': '1220', 'duration': '50' },
+		{ 'cousrName': 'Static Stretrc 静态拉伸', 'TrainName': 'Yiyi', 'Time': '600', 'duration': '50' },
+		{ 'cousrName': 'I Yoga 肩颈深度修复', 'TrainName': 'Jack', 'Time': '1140', 'duration': '50' },
 		{ 'cousrName': 'Sexy Butt 美臀', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
 	],
 
 	[
-		{ 'cousrName': 'Shape yoga 形体瑜伽', 'TrainName': 'Julia', 'Time': '600', 'duration': '50' },
-		{ 'cousrName': 'Jazz爵士舞', 'TrainName': 'David', 'Time': '1220', 'duration': '50' },
-		{ 'cousrName': 'Yin yoga 阴瑜伽', 'TrainName': 'Yiyi', 'Time': '1140', 'duration': '50' },
-		{ 'cousrName': 'CYCLE 脂肪杀手', 'TrainName': 'Queenie', 'Time': '1210', 'duration': '50' },
-		// { 'cousrName': 'Lumbar abdomen 腰腹塑形', 'TrainName': 'Marco', 'Time': '1140', 'duration': '50' },
-	],
-
-	[
-		{ 'cousrName': 'Flow yoga 流瑜伽', 'TrainName': 'Yiyi', 'Time': '840', 'duration': '50' },
-		{ 'cousrName': 'Dance Yoga 舞韵瑜伽', 'TrainName': 'Yur', 'Time': '1140', 'duration': '50' },
-		{ 'cousrName': 'CYCLE 魅力风情', 'TrainName': 'Queenie', 'Time': '1210', 'duration': '50' },
+		{ 'cousrName': 'I Yoga 骨盆保养', 'TrainName': 'Yiyi', 'Time': '840', 'duration': '50' },
+		{ 'cousrName': 'Jazz爵士舞', 'TrainName': 'David', 'Time': '1140', 'duration': '50' },
+		{ 'cousrName': 'Hata Yoga', 'TrainName': 'Yiyi', 'Time': '1180', 'duration': '50' },
+		{ 'cousrName': 'CYCLE 魅力风情', 'TrainName': 'Queenie', 'Time': '1180', 'duration': '50' },
 	]
 ]
