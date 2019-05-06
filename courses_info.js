f3fe8c5eb290326291abffcd77a321c2802130c1
yangkangkai <452306233@qq.com>
2019-05-06 09:35:54 +0800

    20190506

--- courses_info.js
+++ courses_info.js
@@ -3,8 +3,8 @@
 var fjl = [
 	[
 		{ 'cousrName': 'Dance Yoga 舞韵瑜伽', 'TrainName': 'Miya', 'Time': '1140', 'duration': '50' },
-		{ 'cousrName': 'Body Step 有氧踏板', 'TrainName': 'Arno', 'Time': '1140', 'duration': '50' },
-		{ 'cousrName': 'Cycle 脂肪杀手', 'TrainName': 'Arno', 'Time': '1190', 'duration': '50' },
+		{ 'cousrName': 'Body Step 有氧踏板', 'TrainName': 'Arno', 'Time': '1140', 'duration': '50' }, 
+		{ 'cousrName': 'Cycle 脂肪杀手', 'TrainName': 'Arno', 'Time': '1200', 'duration': '50' },
 	],
 
 	[
@@ -15,8 +15,8 @@ var fjl = [
 
 	[
 		{ 'cousrName': 'Mat Pilates 垫上普拉提', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
-		{ 'cousrName': 'Relaxing Strectr 背痛修复', 'TrainName': 'Jack', 'Time': '1140', 'duration': '50' },
-		{ 'cousrName': 'Body Show 魔力瘦', 'TrainName': 'Yiyi', 'Time': '1200', 'duration': '50' },
+		{ 'cousrName': 'Zumba 尊巴', 'TrainName': 'Ice', 'Time': '600', 'duration': '50' },
+		{ 'cousrName': 'I Yoga 精准', 'TrainName': 'Yiyi', 'Time': '1140', 'duration': '50' },
 	],
 
 	[
@@ -37,10 +37,11 @@ var fjl = [
 		{ 'cousrName': 'Static Stretrc 静态拉伸', 'TrainName': 'Yiyi', 'Time': '600', 'duration': '50' },
 		{ 'cousrName': 'I Yoga 肩颈深度修复', 'TrainName': 'Jack', 'Time': '1140', 'duration': '50' },
 		{ 'cousrName': 'Sexy Butt 有氧美臀', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
+		{ 'cousrName': 'Ready to go 激情四射', 'TrainName': 'Baron', 'Time': '1190', 'duration': '50' },
 	],
 
 	[
-		{ 'cousrName': 'Yin yoga 阴瑜伽', 'TrainName': 'Yiyi', 'Time': '840', 'duration': '50' },
+		{ 'cousrName': 'Yin yoga 流瑜伽', 'TrainName': 'Yiyi', 'Time': '840', 'duration': '50' },
 		{ 'cousrName': 'Jazz爵士舞', 'TrainName': 'David', 'Time': '1140', 'duration': '50' },
 		{ 'cousrName': 'Body Show 有氧魔力瘦', 'TrainName': 'Yiyi', 'Time': '1190', 'duration': '50' },
 	]
