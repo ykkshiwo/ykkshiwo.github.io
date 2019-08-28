fae305bc53d50dad6e89b26b62f958c98f51187a
yangkangkai <452306233@qq.com>
2019-08-28 15:30:56 +0800

    教练信息变更

--- trains_info.js
+++ trains_info.js
@@ -131,24 +131,27 @@ var data_person_trains =
 				position: "佑远私人教练",
 				certificates: [
 					"国家职业资格证书",
-					//					"红十字会急救员证",
+					"红十字会急救员证",
 					"功能性训练技能鉴定证书",
 					"普拉提（康复训练）技能鉴定证书",
 					"运动康复3M拉伸技能鉴定证书",
-					//					"拳课技能鉴定证书",
+					"拳课技能鉴定证书",
 				],
 				projects: ["拉伸", "增肌", "减脂", "塑形"]
 			},
 			{
-				en_name: "Alick Li",
+				en_name: "Alick",
 				cn_name: "李文华",
 				img: "http://yy.rxmmd.net/Storage/master/gallery/201908/201908240950214157821.jpg",
 				position: "佑远私人教练",
 				certificates: [
 					"国家职业资格认证",
-					"上海红十字会急救（CPR）认证",
-					"TRX认证",
-					"极致瘦身认证",
+					"AFCC体适能专业认证",
+					"BPTC形体训练认证",
+					"运动康复3M拉伸认证",
+					"Boxing拳击认证",
+					"CPR应急救护认证",
+					"博西斯顿形体雕刻师课认证"
 				],
 				projects: ["增肌", "减脂", "塑形"]
 			},
@@ -160,11 +163,75 @@ var data_person_trains =
 				certificates: [
 					"CPR应急救护",
 					"CPT私人教练认证",
-					//					"体适能认证",
+					"体适能认证",
 					"运动训练师认证",
-					//					"体态评估与矫正",
+					"体态评估与矫正", 
 					"运动复健师认证",
-					"国家体育总局健身教练职业资格认证"
+					"国家体育总局健身教练职业资格认证",
+					"博西斯顿形体雕刻师课认证"
+				],
+				projects: ["减脂", "塑形", "拉伸"]
+			},
+			{
+				en_name: " Kevin",
+				cn_name: "田开拓",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201908/201908240950214157824.jpg",
+				position: "佑远私人教练",
+				certificates: [
+					"卡玛效能精准减脂认证",
+					"AASFP体适能教练认证",
+					"卡玛效能精准拉伸认证",
+					"健身教练国家职业资格证书",
+					"国家社会体育指导员", 
+					"卡玛效能精准训练认证"
+				],
+				projects: ["减脂", "塑形", "拉伸"]
+			},
+			{
+				en_name: " Dana",
+				cn_name: "徐大昌",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201908/201908240950214157825.jpg",
+				position: "佑远私人教练",
+				certificates: [
+					"高级私人教练",
+					"运动康复师",
+					"健康管理师",
+					"雅莲脊美轻柔手法正骨整脊疼痛认证",
+					"华人一手软组织损伤徒手疗法认证", 
+					"毕博士肌筋膜链康复认证",
+					"诺亚第功能评估与远动康复认证",
+					"龙氏正骨治脊疗法认证",
+				],
+				projects: ["疼痛康复", "瘦身塑形", "产后恢复"]
+			},
+			{
+				en_name: " Harry",
+				cn_name: "楼伟",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201908/201908240950214314076.jpg",
+				position: "佑远私人教练",
+				certificates: [
+					"CPR应急救护认证",
+					"CPT拉伸认证",
+					"GFTA体适能教练认证",
+					"EPTC极速瘦身认证",
+					"AASFP体适能教练认证", 
+					"潘敬辉健美训练技术课程认证"
+				],
+				projects: ["减脂", "塑形", "拉伸"]
+			},
+			{
+				en_name: " Arthar",
+				cn_name: "祁筠哲",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201908/201908240950214157822.jpg",
+				position: "佑远私人教练",
+				certificates: [
+					"健身教练国家职业资格证书",
+					"MFT泰拳一级认证",
+					"MFT Boxing Fit 拳击体适能",
+					"WEX 青少年格斗体适能",
+					"JF Boxing 中泰国际一级泰拳认证", 
+					"2017年上海kick boxing 踢拳邀请赛75公斤级冠军",
+					"戴健新健美技术认证"
 				],
 				projects: ["减脂", "塑形", "拉伸"]
 			},
