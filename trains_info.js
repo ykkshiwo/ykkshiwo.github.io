7ce379ea5ebf47c9ac4dc5e704d63239446c56bc
yangkangkai <452306233@qq.com>
2019-08-30 11:39:25 +0800

    教练信息修改

--- trains_info.js
+++ trains_info.js
@@ -6,7 +6,7 @@ var data_group_trains =
 			{
 				en_name: "Julia",
 				cn_name: "未央",
-				position: "佑远团操主管",
+				position: "佑远团操教练",
 				img: "http://yy.rxmmd.net/Storage/master/gallery/201812/201812151011402445200.jpg",
 				certificates: [
 					"理疗契合养生导师",
@@ -17,32 +17,32 @@ var data_group_trains =
 				],
 				projects: ["哈他瑜伽", "阴瑜伽", "流瑜伽", "理疗瑜伽", "高温瑜伽", "心灵疗愈", "阿斯汤加", "普拉提器械", "双语儿童"]
 			},
-			{
-				en_name: "Yiyi",
-				cn_name: "李玮秋",
-				position: "佑远团操教练",
-				img: "http://yy.rxmmd.net/Storage/master/gallery/201811/201811221441194834220.jpg",
-				certificates: [
-					"中国瑜伽师联盟高级瑜伽导师",
-					"阴瑜伽高伽导师认证",
-					"流瑜伽瑜伽导师认证",
-					"高温瑜伽瑜伽导师认证",
-				],
-				projects: ["哈他瑜伽", "阴瑜伽", "流瑜伽", "理疗瑜伽", "高温瑜伽"]
-			},
-			{
-				en_name: "David",
-				cn_name: "大卫",
-				position: "佑远团操教练",
-				img: "http://yy.rxmmd.net/Storage/master/gallery/201902/201902221629157971470.jpg",
-				certificates: [
-					"品诺街舞大赛齐舞季军",
-					"爱跳全国街舞大赛齐舞季军",
-					"各大电视台合作艺人",
-					"北京SDT舞蹈天团演艺部跟团嘉宾"
-				],
-				projects: ["爵士舞"]
-			},
+			// {
+			// 	en_name: "Yiyi",
+			// 	cn_name: "李玮秋",
+			// 	position: "佑远团操教练",
+			// 	img: "http://yy.rxmmd.net/Storage/master/gallery/201811/201811221441194834220.jpg",
+			// 	certificates: [
+			// 		"中国瑜伽师联盟高级瑜伽导师",
+			// 		"阴瑜伽高伽导师认证",
+			// 		"流瑜伽瑜伽导师认证",
+			// 		"高温瑜伽瑜伽导师认证",
+			// 	],
+			// 	projects: ["哈他瑜伽", "阴瑜伽", "流瑜伽", "理疗瑜伽", "高温瑜伽"]
+			// },
+			// {
+			// 	en_name: "David",
+			// 	cn_name: "大卫",
+			// 	position: "佑远团操教练",
+			// 	img: "http://yy.rxmmd.net/Storage/master/gallery/201902/201902221629157971470.jpg",
+			// 	certificates: [
+			// 		"品诺街舞大赛齐舞季军",
+			// 		"爱跳全国街舞大赛齐舞季军",
+			// 		"各大电视台合作艺人",
+			// 		"北京SDT舞蹈天团演艺部跟团嘉宾"
+			// 	],
+			// 	projects: ["爵士舞"]
+			// },
 			{
 				en_name: "Yur",
 				cn_name: "愉儿",
@@ -53,30 +53,30 @@ var data_group_trains =
 				],
 				projects: ["舞韵瑜伽"]
 			},
-			{
-				en_name: "Jack",
-				cn_name: "大海",
-				position: "佑远团操教练",
-				img: "http://yy.rxmmd.net/Storage/master/gallery/201905/201905060938393265080.jpg",
-				certificates: [
-					"哈他瑜伽注册教练",
-					"空中瑜伽注册教练",
-				],
-				projects: ["空中瑜伽", "物理修复"]
-			},
-			{
-				en_name: "Queenie",
-				cn_name: "宋情",
-				position: "佑远团操教练",
-				img: "http://yy.rxmmd.net/Storage/master/gallery/201903/201903041228152111590.jpg",
-				certificates: [
-					"单车注册教练",
-				],
-				projects: ["单车"]
-			},
+			// {
+			// 	en_name: "Jack",
+			// 	cn_name: "大海",
+			// 	position: "佑远团操教练",
+			// 	img: "http://yy.rxmmd.net/Storage/master/gallery/201905/201905060938393265080.jpg",
+			// 	certificates: [
+			// 		"哈他瑜伽注册教练",
+			// 		"空中瑜伽注册教练",
+			// 	],
+			// 	projects: ["空中瑜伽", "物理修复"]
+			// },
+			// {
+			// 	en_name: "Queenie",
+			// 	cn_name: "宋情",
+			// 	position: "佑远团操教练",
+			// 	img: "http://yy.rxmmd.net/Storage/master/gallery/201903/201903041228152111590.jpg",
+			// 	certificates: [
+			// 		"单车注册教练",
+			// 	],
+			// 	projects: ["单车"]
+			// },
 			{
 				en_name: "Baron",
-				cn_name: "男爵",
+				cn_name: "小斌",
 				position: "佑远团操教练",
 				img: "http://yy.rxmmd.net/Storage/master/gallery/201905/201905291234246896350.jpg",  //匿名照片
 				certificates: [
@@ -94,27 +94,27 @@ var data_group_trains =
 				],
 				projects: ["动感单车"]
 			},
+			// {
+			// 	en_name: "Ice",
+			// 	cn_name: "冰冰",
+			// 	position: "佑远团操教练",
+			// 	img: "http://yy.rxmmd.net/Storage/master/gallery/201905/201905060938393265080.jpg",
+			// 	certificates: [
+			// 		"哈他瑜伽注册教练",
+			// 		"空中瑜伽注册教练",
+			// 	],
+			// 	projects: ["瑜伽",]
+			// },
 			{
-				en_name: "Ice",
-				cn_name: "冰冰",
+				en_name: "Amy",
+				cn_name: "阿米",
 				position: "佑远团操教练",
-				img: "http://yy.rxmmd.net/Storage/master/gallery/201905/201905060938393265080.jpg",
+				img: "http://yy.rxmmd.net/Storage/master/gallery/201908/201908301136074568440.jpg",
 				certificates: [
-					"哈他瑜伽注册教练",
-					"空中瑜伽注册教练",
+					// "单车培训资格认证",
+					// "蹦床培训资质证书",
 				],
-				projects: ["瑜伽",]
-			},
-			{
-				en_name: "Arno",
-				cn_name: "阿诺",
-				position: "佑远团操教练",
-				img: "http://yy.rxmmd.net/Storage/master/gallery/201905/201905060938393265080.jpg",
-				certificates: [
-					"单车培训资格认证",
-					"蹦床培训资质证书",
-				],
-				projects: ["瑜伽", "单车", "有氧", "蹦床"]
+				projects: ["单车"]
 			},
 		]
 };
@@ -196,8 +196,8 @@ var data_person_trains =
 					"高级私人教练",
 					"运动康复师",
 					"健康管理师",
-					"雅莲脊美轻柔手法正骨整脊疼痛认证",
-					"华人一手软组织损伤徒手疗法认证", 
+					// "雅莲脊美轻柔手法正骨整脊疼痛认证",
+					// "华人一手软组织损伤徒手疗法认证", 
 					"毕博士肌筋膜链康复认证",
 					"诺亚第功能评估与远动康复认证",
 					"龙氏正骨治脊疗法认证",
@@ -230,7 +230,7 @@ var data_person_trains =
 					"MFT Boxing Fit 拳击体适能",
 					"WEX 青少年格斗体适能",
 					"JF Boxing 中泰国际一级泰拳认证", 
-					"2017年上海kick boxing 踢拳邀请赛75公斤级冠军",
+					// "2017年上海kick boxing 踢拳邀请赛75公斤级冠军",
 					"戴健新健美技术认证"
 				],
 				projects: ["减脂", "塑形", "拉伸"]
