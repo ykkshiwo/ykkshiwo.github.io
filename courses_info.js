b96eb06370275613d5e04a2b4b8905b37cbde667
yangkangkai <452306233@qq.com>
2019-09-01 11:51:27 +0800

    九月课表

--- courses_info.js
+++ courses_info.js
@@ -2,47 +2,58 @@
 //周日在最前面
 var fjl = [
 	[
-		{ 'cousrName': 'Yin yoga 阴瑜伽', 'TrainName': 'Yiyi', 'Time': '1155', 'duration': '50' },
+		// 周日
 		{ 'cousrName': 'Body Step 有氧踏板', 'TrainName': 'Arno', 'Time': '1155', 'duration': '50' },
-		{ 'cousrName': 'Cycle 脂肪杀手', 'TrainName': 'Arno', 'Time': '1210', 'duration': '50' },
+		{ 'cousrName': 'Cycle 脂肪杀手', 'TrainName': 'Arno', 'Time': '1215', 'duration': '50' },
+		{ 'cousrName': 'Power Yoga 流动后弯', 'TrainName': 'Pig', 'Time': '1155', 'duration': '50' },
 	],
 
 	[
-		{ 'cousrName': 'Yin yoga 阴瑜伽', 'TrainName': 'Yiyi', 'Time': '710', 'duration': '60' },
-		{ 'cousrName': 'Mat Pilates 普拉提小球', 'TrainName': 'Julia', 'Time': '1155', 'duration': '50' },
-		{ 'cousrName': 'Ready to go 激情四射', 'TrainName': 'Baron', 'Time': '1170', 'duration': '50' },
+		// 周一
+		{ 'cousrName': 'Mat Pilates 垫上普拉提', 'TrainName': 'Julia', 'Time': '1155', 'duration': '50' },
+		{ 'cousrName': 'Nightclub Cycle 主题课', 'TrainName': '蒋毅', 'Time': '1150', 'duration': '50' },
+		{ 'cousrName': 'Ready to go 激情四射', 'TrainName': 'Baron', 'Time': '1215', 'duration': '50' },
 	],
 
 	[
-		{ 'cousrName': 'Mat Pilates 垫上普拉提', 'TrainName': 'Julia', 'Time': '600', 'duration': '60' },
+		// 周二
+		{ 'cousrName': 'Flow Yoga 流瑜伽', 'TrainName': 'Julia', 'Time': '600', 'duration': '50' },
+		{ 'cousrName': 'Yin Yoga 阴瑜伽', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
+		{ 'cousrName': 'Nightclub Cycle 主题课', 'TrainName': '蒋毅', 'Time': '1150', 'duration': '50' },
 		{ 'cousrName': 'Dance 韩舞', 'TrainName': 'Alice', 'Time': '1155', 'duration': '50' },
 		{ 'cousrName': 'Kendo 剑道', 'TrainName': '周九如', 'Time': '1170', 'duration': '90' },
 		{ 'cousrName': 'Cycle极速飞跃', 'TrainName': 'Amy', 'Time': '1170', 'duration': '50' },
 	],
 
 	[
+		// 周三
 		{ 'cousrName': 'I Yoga 拜日', 'TrainName': 'Julia', 'Time': '600', 'duration': '60' },
-		{ 'cousrName': 'Body Banalce 身心平衡', 'TrainName': 'Ice', 'Time': '710', 'duration': '60' },
-		{ 'cousrName': 'I Yoga 精准', 'TrainName': 'Yiyi', 'Time': '1155', 'duration': '50' },
-		{ 'cousrName': 'Cycle 极速飞跃', 'TrainName': 'Lina', 'Time': '1170', 'duration': '50' },
+		{ 'cousrName': '核心', 'TrainName': '蒋毅', 'Time': '1215', 'duration': '30' },
+		{ 'cousrName': '杠铃', 'TrainName': '蒋毅', 'Time': '1155', 'duration': '50' },
+		{ 'cousrName': 'Cycle 极速飞跃', 'TrainName': 'Lina', 'Time': '1150', 'duration': '50' },
 	],
 
 	[
-		{ 'cousrName': 'Flow Yoga 流瑜伽', 'TrainName': 'Yiyi', 'Time': '600', 'duration': '60' },
-		{ 'cousrName': 'Body Show 有氧魔力瘦', 'TrainName': 'Yiyi', 'Time': '1155', 'duration': '50' },
-		{ 'cousrName': 'Ready to go 决战巅峰', 'TrainName': 'Baron', 'Time': '1170', 'duration': '50' },
+		// 周四
+		{ 'cousrName': 'Power Yoga 力量平衡', 'TrainName': 'Pig', 'Time': '840', 'duration': '50' },
+		{ 'cousrName': 'Nightclub Cycle 主题课', 'TrainName': '蒋毅', 'Time': '1150', 'duration': '50' },
+		{ 'cousrName': 'I Yoga 骨盆修复', 'TrainName': 'Julia', 'Time': '1155', 'duration': '50' },
+		{ 'cousrName': 'Ready to go 决战巅峰', 'TrainName': 'Baron', 'Time': '1215', 'duration': '50' },
 	],
 
 	[
-		{ 'cousrName': 'Static Stretrc 静态拉伸', 'TrainName': 'Yiyi', 'Time': '600', 'duration': '50' },
+		// 周五
+		{ 'cousrName': 'I Yoga 静态拉伸', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
+		{ 'cousrName': 'Nightclub Cycle 主题课', 'TrainName': '蒋毅', 'Time': '1150', 'duration': '50' },
 		{ 'cousrName': 'Gentle Flow 绽放流', 'TrainName': 'Julia', 'Time': '1155', 'duration': '50' },
-		{ 'cousrName': 'Ready to go 激情四射', 'TrainName': 'Baron', 'Time': '1170', 'duration': '50' },
+		{ 'cousrName': 'Ready to go 激情四射', 'TrainName': 'Baron', 'Time': '1215', 'duration': '50' },
 		{ 'cousrName': 'Kendo 剑道', 'TrainName': '周九如', 'Time': '1170', 'duration': '90' },
 	],
 
 	[
-		{ 'cousrName': 'Sexy Butt 有氧美臀', 'TrainName': 'Julia', 'Time': '600', 'duration': '50' },
-		{ 'cousrName': 'Dance Yoga 舞韵瑜伽', 'TrainName': 'Miya', 'Time': '1155', 'duration': '50' },
-		// { 'cousrName': 'Cycle 激情燃烧', 'TrainName': 'Judy', 'Time': '1170', 'duration': '50' },
+		// 周六
+		{ 'cousrName': 'Dance Yoga 舞韵瑜伽', 'TrainName': 'Miya', 'Time': '1150', 'duration': '50' },
+		{ 'cousrName': '核心', 'TrainName': '蒋毅', 'Time': '1215', 'duration': '30' },
+		{ 'cousrName': '杠铃', 'TrainName': '蒋毅', 'Time': '1155', 'duration': '50' },
 	]
 ]
