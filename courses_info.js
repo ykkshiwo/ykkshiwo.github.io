84eff6f4c8cc17e523055f1540bea711fdef00b8
yangkangkai <452306233@qq.com>
2019-10-28 12:04:03 +0800

    11月课程还没有上线

--- courses_info.js
+++ courses_info.js
@@ -9,25 +9,25 @@ var fjl = [
 
 	[
 		// 周一
-		{ 'cousrName': 'Mat Pilates 垫上普拉提', 'TrainName': 'Julia', 'Time': '1210', 'duration': '50' },
-		{ 'cousrName': 'Nightclub Cycle 主题课', 'TrainName': '蒋毅', 'Time': '1170', 'duration': '50' },
-		{ 'cousrName': 'Dance 有氧舞蹈', 'TrainName': 'Anna', 'Time': '1150', 'duration': '50' },
+		// { 'cousrName': 'Mat Pilates 垫上普拉提', 'TrainName': 'Julia', 'Time': '1210', 'duration': '50' },
+		// { 'cousrName': 'Nightclub Cycle 主题课', 'TrainName': '蒋毅', 'Time': '1170', 'duration': '50' },
+		// { 'cousrName': 'Dance 有氧舞蹈', 'TrainName': 'Anna', 'Time': '1150', 'duration': '50' },
 	],
 
 	[
 		// 周二
-		{ 'cousrName': 'Flow Yoga 流瑜伽', 'TrainName': 'Julia', 'Time': '600', 'duration': '50' },
-		{ 'cousrName': 'Nightclub Cycle 主题课', 'TrainName': '蒋毅', 'Time': '1170', 'duration': '50' },
-		{ 'cousrName': 'Dance 韩舞', 'TrainName': 'Alice', 'Time': '1155', 'duration': '50' },
-		{ 'cousrName': 'Power Yoga 肩颈理疗', 'TrainName': '丛姗', 'Time': '1150', 'duration': '50' },
+		// { 'cousrName': 'Flow Yoga 流瑜伽', 'TrainName': 'Julia', 'Time': '600', 'duration': '50' },
+		// { 'cousrName': 'Nightclub Cycle 主题课', 'TrainName': '蒋毅', 'Time': '1170', 'duration': '50' },
+		// { 'cousrName': 'Dance 韩舞', 'TrainName': 'Alice', 'Time': '1155', 'duration': '50' },
+		// { 'cousrName': 'Power Yoga 肩颈理疗', 'TrainName': '丛姗', 'Time': '1150', 'duration': '50' },
 	],
 
 	[
 		// 周三
-		{ 'cousrName': 'Power Yoga 肩颈理疗', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
-		{ 'cousrName': '核心', 'TrainName': '蒋毅', 'Time': '1215', 'duration': '30' },
-		{ 'cousrName': '杠铃', 'TrainName': '蒋毅', 'Time': '1155', 'duration': '50' },
-		{ 'cousrName': 'Nightclub Cycle 主题课', 'TrainName': '辛战勇', 'Time': '1170', 'duration': '50' },
+		// { 'cousrName': 'Power Yoga 肩颈理疗', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
+		// { 'cousrName': '核心', 'TrainName': '蒋毅', 'Time': '1215', 'duration': '30' },
+		// { 'cousrName': '杠铃', 'TrainName': '蒋毅', 'Time': '1155', 'duration': '50' },
+		// { 'cousrName': 'Nightclub Cycle 主题课', 'TrainName': '辛战勇', 'Time': '1170', 'duration': '50' },
 	],
 
 	[
