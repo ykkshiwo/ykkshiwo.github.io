c5cc61adecd63fe093187b8918d997b34ef098a0
yangkangkai <452306233@qq.com>
2020-08-30 19:55:10 +0800

    999

--- courses_info.js
+++ courses_info.js
@@ -3,7 +3,7 @@
 var fjl = [
 	[
 		// 周日
-		{ 'cousrName': 'Cycle 极速减脂', 'TrainName': '蒋毅', 'Time': '1170', 'duration': '45' },
+		{ 'cousrName': 'Cycle 心肺训练', 'TrainName': '蒋毅', 'Time': '1170', 'duration': '45' },
 	],
 
 	[
@@ -12,38 +12,38 @@ var fjl = [
 		// { 'cousrName': 'Dance 有氧舞蹈', 'TrainName': 'Anna', 'Time': '1140', 'duration': '50' },
 		// { 'cousrName': 'Mat Pilates 垫上普拉提', 'TrainName': 'Julia', 'Time': '1160', 'duration': '50' },
 		// { 'cousrName': '小团课', 'TrainName': '猎豹', 'Time': '1170', 'duration': '40' },
-		{ 'cousrName': 'I Yoga 瑜伽', 'TrainName': 'Julia', 'Time': '1170', 'duration': '50' },
+		{ 'cousrName': 'Yoga 哈他瑜伽', 'TrainName': '洛熙', 'Time': '1170', 'duration': '50' },
 	],
 
 	[
 		// 周二
 		//{ 'cousrName': 'Flow Yoga 流瑜伽', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
 		//{ 'cousrName': '肩颈理疗', 'TrainName': '丛姗', 'Time': '1140', 'duration': '50' },
-		// { 'cousrName': 'Cycle 开心一刻', 'TrainName': 'Jony', 'Time': '1170', 'duration': '50' },
+		{ 'cousrName': 'Cycle 开心一刻', 'TrainName': 'Jony', 'Time': '1170', 'duration': '50' },
 		// { 'cousrName': 'Dance 尊巴', 'TrainName': 'Anna', 'Time': '1200', 'duration': '50' },
-		{ 'cousrName': 'Cycle 极速减脂', 'TrainName': '蒋毅', 'Time': '1170', 'duration': '45' },
+		{ 'cousrName': 'Mat Pilates 垫上普拉提', 'TrainName': 'Julia', 'Time': '1160', 'duration': '50' },
 	],
 
 	[
 		// 周三
 		// { 'cousrName': '瑜伽YOGA', 'TrainName': 'Julia', 'Time': '1170', 'duration': '50' },
-		{ 'cousrName': 'Mat Pilates 垫上普拉提', 'TrainName': 'Julia', 'Time': '1170', 'duration': '50' },
-		// { 'cousrName': 'pump杠铃', 'TrainName': '蒋毅', 'Time': '1155', 'duration': '50' },
-		// { 'cousrName': '核心', 'TrainName': '蒋毅', 'Time': '1215', 'duration': '30' },
+		// { 'cousrName': 'Mat Pilates 垫上普拉提', 'TrainName': 'Julia', 'Time': '1170', 'duration': '50' },
+		{ 'cousrName': 'PUMP杠铃', 'TrainName': '蒋毅', 'Time': '1155', 'duration': '45' },
+		{ 'cousrName': 'CX核心训练', 'TrainName': '蒋毅', 'Time': '1215', 'duration': '30' },
 	],
 
 	[
 		// 周四
 		//{ 'cousrName': 'I Yoga 拜日', 'TrainName': 'Julia', 'Time': '840', 'duration': '50' },
-		{ 'cousrName': 'Cycle 疯狂燃脂', 'TrainName': '蒋毅', 'Time': '1170', 'duratio': '45' },
+		{ 'cousrName': 'Cycle 狂欢之夜', 'TrainName': 'Jony', 'Time': '1170', 'duratio': '45' },
 		//{ 'cousrName': '私教小团课', 'TrainName': '猎豹&老A', 'Time': '1170', 'duration': '45' },
 		//{ 'cousrName': 'I Yoga 骨盆修复', 'TrainName': 'Julia', 'Time': '1200', 'duration': '50' },
 	],
 
 	[
 		// 周五
-		{ 'cousrName': 'I Yoga 瑜伽', 'TrainName': 'Julia', 'Time': '1170', 'duration': '50' },
-		{ 'cousrName': 'Cycle 极速飞轮', 'TrainName': 'Stella', 'Time': '1170', 'duratio': '45' },
+		{ 'cousrName': 'I Yoga 骨盆修复', 'TrainName': 'Julia', 'Time': '1170', 'duration': '50' },
+		{ 'cousrName': 'Cycle 激情速度', 'TrainName': 'Stella', 'Time': '1170', 'duratio': '45' },
 		// { 'cousrName': '杠铃', 'TrainName': '蒋毅', 'Time': '1155', 'duration': '50' },
 		// { 'cousrName': '核心', 'TrainName': '蒋毅', 'Time': '1215', 'duration': '30' },
 	],
